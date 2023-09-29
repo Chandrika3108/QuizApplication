ce5aa098b217828305fdeb2b745378e99d28c89b
Chandrika3108 <chandrikagadiparthi7@gmail.com>
2023-09-29 17:25:25 -0400

    Changed from name to username

--- src/main/java/com/quizApp/quizApplication/dao/UserDAO.java
+++ src/main/java/com/quizApp/quizApplication/dao/UserDAO.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface UserDAO extends CrudRepository<User, Long> {
 
     List<User> findByEmail(String email);
-
+    List<User> findByUsername(String username);
 }
